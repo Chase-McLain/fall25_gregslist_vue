a63da41cd3999d67f7237a6a40a24dbe51434362
Chase McLain <chasermclain@gmail.com>
2025-09-17 13:52:40 -0600

    pet model

---
+++ src/models/Pet.js
@@ -0,0 +1,29 @@
+
+
+
+export class Pet{
+  constructor(data){
+
+    this.name = data.name
+    this.age = data.age
+    this.isVaccinated = data.isVaccinated
+    this.species = data.species
+    this.status = data.status
+    this.imgUrl = data.imgUrl
+    this.likes = data.likes
+    this.creatorId = data.creatorId
+    this.id = data.id
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
