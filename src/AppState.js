1953a714194c6b29492860c63d58e918af74aed5
Chase McLain <chasermclain@gmail.com>
2025-09-17 13:53:54 -0600

    appstate pets added

--- src/AppState.js
+++ src/AppState.js
@@ -2,11 +2,18 @@ import { reactive } from 'vue'
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
+
   /**@type {import('@bcwdev/auth0provider-client').Identity} */
   identity: null,
+
   /** @type {import('./models/Account.js').Account} user info from the database*/
   account: null,
+
   /** @type {import('./models/Car.js').Car[]} */
-  cars: []
+  cars: [],
+
+  /** @type {import('./models/Pet.js').Pet[]} */
+  pets: []
 })
+
 
