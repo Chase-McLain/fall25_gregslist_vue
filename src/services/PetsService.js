ad7a1d925980a717e95fe9f5c9a16aa84d72c0cf
Chase McLain <chasermclain@gmail.com>
2025-09-17 14:14:46 -0600

    getting pets

--- src/services/PetsService.js
+++ src/services/PetsService.js
@@ -1,11 +1,19 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
+import { AppState } from "@/AppState.js"
+import { Pet } from "@/models/Pet.js"
 
 
 
 
 
 class PetsService{
-  getPets() {
-    throw new Error('Method not implemented.');
+ async getPets() {
+    const response = await api.get('api/pets')
+    logger.log(response)
+    const pets = response.data.map((pet)=> new Pet(pet))
+    AppState.pets = pets
+    logger.log(AppState.pets)
   }
 
 
